38d8948b4c98056c1190c9b8e58135619c86e4aa
Dobrovenskis <dobrovenskis.rv@phystech.edu>
2021-12-04 23:29:02 +0300

    made the interaction of balls

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 #include <vector>
 //#include "settings.cpp"
 
+
 float speed_player = 0.1;
 int wind_x_size = 1000;
 int wind_y_size = 700;
 float rad_ball = 20;
 float dt = 0.01;
-float betta_tormoz = 0.02;
+float betta_tormoz = 0.1;
 sf::RenderWindow window(sf::VideoMode(wind_x_size, wind_y_size), "SFML works!");
 
 float field_y_size = 100; // в процентах от wind_y_size
@@ -49,6 +50,30 @@ std::vector<T> operator*(T skal, std::vector<T> vec1)
     std::transform(vec1.begin(), vec1.end(), prod.begin(), [skal](T& el) { return el * skal; });
     return prod;
 }
+
+template <typename T>
+T skalar_prod(std::vector<T> vec1, std::vector<T> vec2)
+{
+    T sum = 0;
+    for (int i = 0; i < vec1.size(); i++)
+    {
+        sum += vec1[i] * vec2[i];
+    }
+    return sum;
+}
+
+template <typename T>
+T norm_squared(std::vector<T> vec)
+{
+    return skalar_prod(vec, vec);
+}
+
+//template <typename T>
+float norm(std::vector<float> vec)
+{
+    return sqrt(norm_squared(vec));
+}
+
 
 template <typename T>
 void print_vec(std::vector<T> vec)
@@ -90,19 +115,38 @@ public:
         std::cout << "field_x1 " << field_x1;
     };
 
+    std::vector<float> ret_pos()
+    {
+        return pos;
+    }
+
+    std::vector<float> ret_speed()
+    {
+        return V;
+    }
+
     void draw()
     {
         window.draw(shape);
     };
 
-    void move()
+    void move(std::vector<Ball> balls)
     {
-        pos = pos + V * dt;
-        V = V + a * dt;
+        
+        
         a = float(-1) * V * betta_tormoz;
-        colapse_wall();
+        interaction(balls);
         shape.setPosition(pos[0] - rad, pos[1] - rad);
+        V = V + a * dt;
+
+        pos = pos + V * dt;
     };
+
+    void interaction(std::vector<Ball> balls)
+    {
+        colapse_with_all_balls(balls);
+        colapse_wall();
+    }
 
     void colapse_wall()
     {
@@ -131,6 +175,34 @@ public:
         }
     }
 
+    void colapse_with_all_balls(std::vector<Ball> balls)
+    {
+        for (Ball ball2 : balls)
+        {
+            if (ball2.pos != pos)
+            {
+                std::vector<float> x1 = pos;
+                std::vector<float> x2 = ball2.ret_pos();
+                //print_vec(x1 - x2);
+                //std::cout << " " << norm_squared(x1 - x2) << "\n";
+                if (norm(x1 - x2) <= 2 * rad)
+                {
+                    std::cout << "COLAPSE";
+                    colapse_ball(ball2);
+                }
+            }
+        }
+    }
+
+    void colapse_ball(Ball ball2)
+    {
+        std::vector<float> V1 = V;
+        std::vector<float> V2 = ball2.ret_speed();
+        std::vector<float> x1 = pos;
+        std::vector<float> x2 = ball2.ret_pos();  
+        V = V - skalar_prod(V1 - V2, x1 - x2) / norm_squared(x1 - x2) * (x1 - x2);
+    }
+
     void set_V(std::vector<float> new_V )
     {
         V = new_V;
@@ -143,10 +215,24 @@ int main()
     field.setSize(sf::Vector2f(field_x_size/100 * wind_x_size, field_y_size / 100 * wind_y_size));
     field.setPosition(wind_x_size / 2 - field_x_size/100 * wind_x_size / 2, 0);
     
-    std::vector<float> pos1{ 500, 100 };
-    std::vector<float> V1{ 50, 100 };
+    std::vector<float> pos1{ 300, 100 };
+    std::vector<float> V1{ 10, 10 };
     Ball ball1(pos1, field);
     ball1.set_V(V1);
+    std::vector<float> pos2{ 400, 50 };
+    std::vector<float> V2{ 5, 10 };
+    Ball ball2(pos2, field);
+    ball2.set_V(V2);
+    std::vector<float> pos3{ 400, 200 };
+    std::vector<float> V3{ 0, -10 };
+    Ball ball3(pos3, field);
+    ball3.set_V(V3);
+    std::vector<float> pos4{ 400, 600 };
+    std::vector<float> V4{ 300, -100 };
+    Ball ball4(pos4, field);
+    ball4.set_V(V4);
+
+    std::vector<Ball> balls = { ball1, ball2, ball3, ball4 };
 
     while (window.isOpen())
     {
@@ -165,13 +251,22 @@ int main()
         {
             //pl.move(speed_player * (1), 0);
         }
-
-        ball1.move();
+        
+        std::vector<Ball> new_balls;
+        for (Ball main_ball : balls)
+        {
+            main_ball.move(balls);
+            new_balls.push_back(main_ball);
+        }
+        balls = new_balls;
        
 
         window.clear();
         window.draw(field);
-        ball1.draw();
+        for (Ball ball : balls)
+        {
+            ball.draw();
+        }
         window.display();
     }
 
