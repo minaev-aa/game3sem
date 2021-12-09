58b569664f5b176c60e24026958e2941d9082650
minaev-aa <71834110+minaev-aa@users.noreply.github.com>
2021-12-09 23:02:01 +0300

    add menu + gui

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <array>
 #include <algorithm>
+#include <array>
+#include <iostream>
 #include <math.h>
 #include <vector>
-#include <cmath>
+#include <SFML/Graphics.hpp>
+//#include <SFML/System.hpp>
+//#include <SFML/Window.hpp>
 //#include "settings.cpp"
 
 
@@ -15,9 +16,11 @@ float rad_ball = 20; //20
 float dt = 0.01;
 float betta_tormoz = 0.1; //0.1
 float tormoz_V = 0.8;
-float time_of_full_beat = 1.0;
-float max_force_of_beat = 100;
-sf::RenderWindow window(sf::VideoMode(wind_x_size, wind_y_size), "SFML works!");
+float time_of_full_beat = 0.7;
+float max_force_of_beat = 1150;
+sf::VideoMode		videoMode = sf::VideoMode(wind_x_size, wind_y_size);
+sf::RenderWindow* window = new sf::RenderWindow(videoMode, "Billiarrd", sf::Style::Default);
+
 
 float field_y_size = 100; // в процентах от wind_y_size
 float field_x_size = 0.4 * field_y_size;
@@ -88,6 +91,263 @@ void print_vec(std::vector<T> vec)
     }
 }
 
+class Button: public sf::Drawable
+{
+public:
+	bool pressed;
+    Button(std::string str, sf::Vector2f pos)
+    {
+        shape.setPosition(pos);
+        shape.setSize(sf::Vector2f(200.f, 50.f));
+        shape.setFillColor(sf::Color(0, 0, 0, 150));
+        font.loadFromFile("C:/Users/ponam/Documents/PFAgoraSlabPro.ttf");
+        text.setFont(font);
+        text.setCharacterSize(30);
+        text.setString(str);
+        text.setPosition(sf::Vector2f(
+            shape.getPosition().x + (shape.getLocalBounds().width / 2.f - text.getLocalBounds().width / 2.f),
+            shape.getPosition().y + (shape.getLocalBounds().height / 2.f - text.getLocalBounds().height)));
+        pressed = false;
+    }
+    ~Button(){}
+
+    void setPosition(float x, float y)
+    {
+        shape.setPosition(x, y);
+        text.setPosition(shape.getPosition().x + 10.f, shape.getPosition().y + 10.f);
+    }
+    bool mouseCheck(sf::Vector2f mouse)
+    {
+        if (shape.getGlobalBounds().left <= mouse.x &&
+            mouse.x <= (shape.getGlobalBounds().left + shape.getGlobalBounds().width) &&
+            shape.getGlobalBounds().top <= mouse.y &&
+            mouse.y <= (shape.getGlobalBounds().top + shape.getGlobalBounds().height))
+        {
+            shape.setFillColor(sf::Color(0, 90, 170, 180));
+            return true;
+        }
+
+        shape.setFillColor(sf::Color(0, 0, 0, 150));
+        return false;
+    }
+    
+    void updateEvent(sf::Vector2f mouse, sf::Event event)
+    {
+        if (event.mouseButton.button == sf::Mouse::Left && mouseCheck(mouse))
+        {
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                shape.setOutlineThickness(-2);
+                pressed = true;
+            }
+            if (event.type == sf::Event::MouseButtonReleased)
+            {
+                shape.setOutlineThickness(0);
+                pressed = false;
+            }
+        }
+    }
+    void update(sf::Vector2f mouse, sf::Event event)
+    {
+        mouseCheck(mouse);
+        updateEvent(mouse, event);
+    }
+
+    enum btnType
+    {
+        CONTINUE,
+        NEW_GAME,
+        EXIT
+    };
+
+private:
+    sf::RectangleShape shape;
+    sf::Text text;
+    sf::Font font;
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        target.draw(shape);
+        target.draw(text);
+    }
+};
+
+class Menu: public sf::Drawable
+{
+public:
+    std::map <Button::btnType, Button*> buttons;
+    Menu() {
+        background.setSize(sf::Vector2f(400.f, 450.f));
+        background.setPosition(300.f, 125.f);
+        background.setFillColor(sf::Color(0, 100, 0, 100));
+
+        Button* btnContinue = new Button("Continue", sf::Vector2f(400.f, 150.f));
+        Button* btnNewGame = new Button("New Game", sf::Vector2f(400.f, 250.f));
+        Button* btnExit = new Button("Exit", sf::Vector2f(400.f, 450.f));
+        buttons.insert(std::pair<Button::btnType, Button*>(Button::CONTINUE, btnContinue));
+        buttons.insert(std::pair<Button::btnType, Button*>(Button::NEW_GAME, btnNewGame));
+        buttons.insert(std::pair<Button::btnType, Button*>(Button::EXIT, btnExit));
+    }
+    ~Menu()
+    {
+        buttons.clear();
+    }
+
+    virtual void update(sf::RenderWindow* window, bool& isPaused, bool& isRestart, sf::Vector2f mouse, sf::Event event)
+    {
+        for (auto btn : buttons)
+        {
+            if (btn.first == Button::EXIT && btn.second->pressed)
+            {
+                window->close();
+            }
+            if (btn.first == Button::CONTINUE && btn.second->pressed)
+            {
+                isPaused = false;
+            }
+            if (btn.first == Button::NEW_GAME && btn.second->pressed)
+            {
+                isRestart = true;
+            }
+            btn.second->update(mouse, event);
+        }
+    }
+
+private:
+    sf::RectangleShape background;
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        target.draw(background);
+        for (auto btn : buttons)
+        {
+            target.draw(*btn.second);
+        }
+    }
+};
+class GUI : public sf::Drawable
+{
+public:
+    bool gamePaused;
+    sf::Font font;
+    Menu menu;
+
+    GUI() {
+        font.loadFromFile("C:/Users/ponam/Documents/PFAgoraSlabPro.ttf");
+
+        player1.points = 0;
+        player1.number = 0;
+        player1.numberText.setFont(font);
+        player1.pointsText.setFont(font);
+        player1.numberText.setString("Player 1");
+        player1.numberText.setPosition(100, 100);
+        player1.numberText.setCharacterSize(40);
+        player1.pointsText.setCharacterSize(35);
+        player1.pointsText.setString(std::to_string(player1.points));
+        player1.pointsText.setPosition(150, 170);
+        player2.points = 0;
+        player2.number = 1;
+        player2.numberText.setFont(font);
+        player2.pointsText.setFont(font);
+        player2.numberText.setString("Player 2\n");
+        player2.numberText.setPosition(750, 100);
+        player2.numberText.setCharacterSize(40);
+        player2.pointsText.setCharacterSize(35);
+        player2.pointsText.setString(std::to_string(player2.points));
+        player2.pointsText.setPosition(800, 170);
+
+        currentPlayer = player1;
+        gamePaused = false;
+    }
+    ~GUI() {}
+
+    struct Player
+    {
+        sf::Text numberText;
+        sf::Text pointsText;
+        int points;
+        int number;
+    };
+
+    Player player1;
+    Player player2;
+    Player currentPlayer;
+
+    void setCurrentPlayer(Player player)
+    {
+        currentPlayer = player;
+    }
+    void setCurrentPlayer(int number)
+    {
+        if (number == 0)
+        {
+            currentPlayer = player1;
+        }
+        if (number == 1)
+        {
+            currentPlayer = player2;
+        }
+    }
+    Player getCurrentPlayer()
+    {
+        return currentPlayer;
+    }
+    void addPoints(Player player)
+    {
+        if (player.number == 0)
+        {
+            player1.points++;
+        }
+        if (player.number == 1)
+        {
+            player2.points++;
+        }
+    }
+
+    void restart()
+    {
+        gamePaused = false;
+        player1.points = 0;
+        player2.points = 0;
+        currentPlayer = player1;
+    }
+    virtual void update(sf::RenderWindow* window, bool& isPaused, bool& isRestart, sf::Vector2f mouse, sf::Event event)
+    {
+        player1.numberText.setString("Player 1");
+        player1.pointsText.setString(std::to_string(player1.points));
+        player2.numberText.setString("Player 2");
+        player2.pointsText.setString(std::to_string(player2.points));
+        player1.numberText.setOutlineThickness(0.f);
+        player2.numberText.setOutlineThickness(0.f);
+        if (currentPlayer.number == 0)
+        {
+            player1.numberText.setOutlineThickness(3.f);
+            player1.numberText.setOutlineColor(sf::Color(0, 0, 0, 100));
+        }
+        if (currentPlayer.number == 1)
+        {
+            player2.numberText.setOutlineThickness(3.f);
+            player2.numberText.setOutlineColor(sf::Color(0, 0, 0, 100));
+        }
+
+        menu.update(window, isPaused, isRestart, mouse, event);
+    }
+
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+
+        if (gamePaused)
+        {
+            target.draw(menu);
+        }
+
+        target.draw(player1.numberText);
+        target.draw(player1.pointsText);
+        target.draw(player2.numberText);
+        target.draw(player2.pointsText);
+    }
+
+
+};
 
 class Ball
 {
@@ -130,13 +390,13 @@ public:
 
     void draw()
     {
-        window.draw(shape);
+        window->draw(shape);
     };
 
     void move(std::vector<Ball> balls)
     {
-        
-        
+
+
         a = float(-1) * V * betta_tormoz;
         interaction(balls);
         shape.setPosition(pos[0] - rad, pos[1] - rad);
@@ -190,10 +450,10 @@ public:
             {
                 std::vector<float> x1 = pos;
                 std::vector<float> x2 = ball2.ret_pos();
-                
+
                 if (norm(x1 - x2) <= 2 * rad)
                 {
-                    //std::cout << "COLAPSE";
+                    std::cout << "COLAPSE";
                     colapse_ball(ball2);
                 }
             }
@@ -205,34 +465,39 @@ public:
         std::vector<float> V1 = V;
         std::vector<float> V2 = ball2.ret_speed();
         std::vector<float> x1 = pos;
-        std::vector<float> x2 = ball2.ret_pos(); 
+        std::vector<float> x2 = ball2.ret_pos();
         if (skalar_prod(V2 - V1, x1 - x2) >= 0)
         {
             V = V - skalar_prod(V1 - V2, x1 - x2) / norm_squared(x1 - x2) * (x1 - x2);
         }
     }
 
-    void set_V(std::vector<float> new_V )
-    {
-        V = new_V;
-    }
-
     bool is_clicked(std::vector <float> mouse_pos)
     {
         return norm(pos - mouse_pos) < rad_ball;
     }
+
+    void set_V(std::vector<float> new_V)
+    {
+        V = new_V;
+    }
 };
+
+
+
 
 int main()
 {
+   
+    sf::Vector2f		m_mouse;
     sf::RectangleShape field;
     field.setSize(sf::Vector2f(field_x_size/100 * wind_x_size, field_y_size / 100 * wind_y_size));
     field.setPosition(wind_x_size / 2 - field_x_size/100 * wind_x_size / 2, 0);
-
+    
     std::vector <float> mouse_pos{ -10, -10 };
     int selected_ball = -1; //значение -1, если не выбран.
     float force_of_beat = 0;
-    
+
     std::vector<float> pos1{ 300, 100 };
     std::vector<float> V1{ 10, 10 };
     Ball ball1(pos1, field);
@@ -251,70 +516,102 @@ int main()
     ball4.set_V(V4);
 
     std::vector<Ball> balls = { ball1, ball2, ball3, ball4 };
-
-    while (window.isOpen())
+    bool restartGame = false;
+    GUI* gui;
+    gui = new GUI();
+    while (window->isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            
-        }
-
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-        {
-            if (force_of_beat < time_of_full_beat){
-                force_of_beat += dt;
-            }
-   
-            for (int i = 0; i < balls.size(); i++)
-            {
-                sf::Vector2i pos_m = sf::Mouse::getPosition(window);
-                mouse_pos[0] = pos_m.x;
-                mouse_pos[1] = pos_m.y;
-                if (balls[i].is_clicked(mouse_pos)) { selected_ball = i; }
-            }
-        }
-        else
-        {
-            if (selected_ball != -1) 
-            {
-                std::cout << "Beat!";
-                std::vector <float> beat{ 0,0 };
-                beat = ((balls[selected_ball].ret_pos() - mouse_pos) * (1/norm(balls[selected_ball].ret_pos() - mouse_pos))) * force_of_beat * max_force_of_beat;
-                balls[selected_ball].set_V(balls[selected_ball].ret_speed() + beat);
-            //beat
-            }
-                
-            selected_ball = -1;
-            force_of_beat = 0;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            //pl.move(speed_player*(-1), 0);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            //pl.move(speed_player * (1), 0);
-        }
         
-        std::vector<Ball> new_balls;
-        for (Ball main_ball : balls)
-        {
-            main_ball.move(balls);
-            new_balls.push_back(main_ball);
+        if (!gui->gamePaused){
+	        std::vector<Ball> new_balls;
+	        for (Ball main_ball : balls)
+	        {
+	            main_ball.move(balls);
+	            new_balls.push_back(main_ball);
+                /*if (x)  //Добавить булевскую элемент забития мяча
+                {
+                    gui->addPoints(gui->getCurrentPlayer());
+                    x = false;
+                }*/
+                if (main_ball.ret_speed() == std::vector<float>(2))
+                {
+                    gui->setCurrentPlayer(1 - gui->getCurrentPlayer().number);
+                }
+	        }
+	        balls = new_balls;
+            window->clear(sf::Color(75, 0, 163));
+	        window->draw(field);
+            for (Ball ball : balls)
+            {
+                ball.draw();
+            }
         }
-        balls = new_balls;
-       
 
-        window.clear();
-        window.draw(field);
-        for (Ball ball : balls)
+        if (restartGame)
         {
-            ball.draw();
+            gui->restart();
+            restartGame = false;
+            //надо всё вернуть в исходное
         }
-        window.display();
+
+        sf::Event   sfmlEvent;
+        while (window->pollEvent(sfmlEvent))
+        {
+            //можно добавить управление шариком
+            switch (sfmlEvent.type)
+            {
+            case sf::Event::Closed:
+                window->close();
+                break;
+            case sf::Event::KeyPressed:
+                if (sfmlEvent.key.code == sf::Keyboard::Escape)
+                {
+                    std::cout << 45678;
+                    gui->gamePaused = 1 - gui->gamePaused;
+                }
+
+            case sf::Event::MouseMoved:
+                m_mouse.x = sfmlEvent.mouseMove.x;
+                m_mouse.y = sfmlEvent.mouseMove.y;
+
+            case sf::Event::MouseButtonPressed:
+                if (true) {
+                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                    {
+                        if (force_of_beat < time_of_full_beat) {
+                            force_of_beat += dt;
+                        }
+
+                        for (int i = 0; i < balls.size(); i++)
+                        {
+                            //sf::Vector2i pos_m = sf::Mouse::getPosition(window);
+                            mouse_pos[0] = m_mouse.x;
+                            mouse_pos[1] = m_mouse.y;
+                            if (balls[i].is_clicked(mouse_pos)) { selected_ball = i; }
+                        }
+                    }
+                    else
+                    {
+                        if (selected_ball != -1)
+                        {
+                            std::cout << "Beat!";
+                            std::vector <float> beat{ 0,0 };
+                            beat = ((balls[selected_ball].ret_pos() - mouse_pos) * (1 / norm(balls[selected_ball].ret_pos() - mouse_pos))) * force_of_beat * max_force_of_beat;
+                            balls[selected_ball].set_V(balls[selected_ball].ret_speed() + beat);
+                            //beat
+                        }
+
+                        selected_ball = -1;
+                        force_of_beat = 0;
+                    }
+                }
+            }
+        }
+
+		gui->update(window, gui->gamePaused, restartGame, m_mouse, sfmlEvent);
+        window->draw(*gui);
+        
+        window->display();
     }
 
     return 0;
